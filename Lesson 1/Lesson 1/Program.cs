0b1e39f70a5c2095eb14a34030c15e30e6839805
BethCushnie <github@bethcushnie.com>
2020-03-03 21:08:12 -0500

    Experimenting
    Figuring out how different things work by trying new things.

--- Lesson 1/Lesson 1/Program.cs
+++ Lesson 1/Lesson 1/Program.cs
@@ -4,9 +4,11 @@ namespace Lesson_1
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("I'm still kinda lost but optimitic ✌");
+            Console.WriteLine("Will this work? Jimmy knows but he's making me learn like a loser");
+            Console.WriteLine("lmao I forgot a semicolon 10/10 start");
         }
     }
 }
