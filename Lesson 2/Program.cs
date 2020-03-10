b0c01ea2c659d995586bc3b6c2b8174abd813401
BethCushnie <github@bethcushnie.com>
2020-03-10 19:50:41 -0400

    Introducing Variables

--- Lesson 2/Program.cs
+++ Lesson 2/Program.cs
@@ -8,6 +8,9 @@ namespace Lesson_2
         static void Main()
         {
             string text = "hi blog peeps";
+            // Variable: an instance of an object. "Text" is a variable, but it can be anything
+            // Useful if you want to use the same string multiple times (now you can just write "text")
+            // Also, if you want to change the string, you only have to do it in one place
             Console.WriteLine(text);
         }
     }
