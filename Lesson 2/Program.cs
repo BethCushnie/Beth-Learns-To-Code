6783d39ef50a3225d06cf3f657b00cae1a3a1325
BethCushnie <github@bethcushnie.com>
2020-03-10 19:58:43 -0400

    Some shorthands

--- Lesson 2/Program.cs
+++ Lesson 2/Program.cs
@@ -11,6 +11,15 @@ namespace Lesson_2
             // Variable: an instance of an object. "Text" is a variable, but it can be anything
             // Useful if you want to use the same string multiple times (now you can just write "text")
             // Also, if you want to change the string, you only have to do it in one place
+
+            text += " a new string";
+            // text = text + " a new string";
+            // += is shorthand for saying "old variable plus whatever comes in front of the operator (whatever comes next)
+            // It updates the variable
+
+            // text -= " a thrid string";
+            // this makes an error but you can also minus a section
+            // Also have *= and /=
             Console.WriteLine(text);
         }
     }
