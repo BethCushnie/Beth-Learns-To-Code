b9148a1683b683d7d18d2f20c2e2f51bd0935382
BethCushnie <github@bethcushnie.com>
2020-03-10 20:57:12 -0400

    learning about TryParse

--- Lesson 2/Program.cs
+++ Lesson 2/Program.cs
@@ -6,17 +6,19 @@ namespace Lesson_2
     {
         static void Main()
         {
-            Console.ForegroundColor = ConsoleColor.Cyan;
-           
+            Console.ForegroundColor = ConsoleColor.Green;
+
             Console.WriteLine("Type a number!");
             string line = Console.ReadLine();
-            int number = int.Parse(line);
-          
+            bool lineIsValid = int.TryParse(line, out int number);
+            // int.TryParse works like int.Parse (converts line to number) except it doesn't fail if the input isn't valid
+            // if line isn't a number, instead of the program crashing, it just leaves Number as 0
+
             Console.WriteLine("Type another number!");
             string line2 = Console.ReadLine();
             int number2 = int.Parse(line2);
 
-            int answer = number - number2;
+            int answer = number + number2;
             Console.WriteLine($"The answer is {answer}");
         }
     }
