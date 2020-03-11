ae4a7e6ca10090d88b37ad528a8e4ce162be6cbe
BethCushnie <github@bethcushnie.com>
2020-03-10 21:05:14 -0400

    inverting boolians?

--- Lesson 2/Program.cs
+++ Lesson 2/Program.cs
@@ -11,19 +11,14 @@ namespace Lesson_2
             Console.WriteLine("Type a number!");
             string line = Console.ReadLine();
             bool lineIsValid = int.TryParse(line, out int number);
-            // int.TryParse works like int.Parse (converts line to number) except it doesn't fail if the input isn't valid
-            // if line isn't a number, instead of the program crashing, it just leaves Number as 0
 
 
-            if (lineIsValid)
-            {
-                Console.WriteLine("input was valid!");
-                // remember: "" declare it as a string
-            }
-            else
+            if (!lineIsValid)
+                // if lineIsValid is false
             {
                 Console.WriteLine("u done f***ed up");
             }
+            //this doesn't say anything now if the input is valid
 
 
             Console.WriteLine("Type another number!");
