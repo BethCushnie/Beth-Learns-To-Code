49eea00ede2f7a7160d63d050e274b139fe8f3c3
BethCushnie <github@bethcushnie.com>
2020-03-10 21:03:11 -0400

    branching and boolians

--- Lesson 2/Program.cs
+++ Lesson 2/Program.cs
@@ -14,6 +14,18 @@ namespace Lesson_2
             // int.TryParse works like int.Parse (converts line to number) except it doesn't fail if the input isn't valid
             // if line isn't a number, instead of the program crashing, it just leaves Number as 0
 
+
+            if (lineIsValid)
+            {
+                Console.WriteLine("input was valid!");
+                // remember: "" declare it as a string
+            }
+            else
+            {
+                Console.WriteLine("u done f***ed up");
+            }
+
+
             Console.WriteLine("Type another number!");
             string line2 = Console.ReadLine();
             int number2 = int.Parse(line2);
