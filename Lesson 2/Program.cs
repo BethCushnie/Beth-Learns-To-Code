faf78f16f4bc0b1838f8488eaf04a7cbf5a6ff03
BethCushnie <github@bethcushnie.com>
2020-03-10 20:06:49 -0400

    Learn about functions that return a value

--- Lesson 2/Program.cs
+++ Lesson 2/Program.cs
@@ -1,26 +1,17 @@
 ﻿using System;
-using System.Collections;
 
 namespace Lesson_2
 {
     class Program
     {
         static void Main()
+            // void means that the function does not return code, it only runs it
         {
-            string text = "hi blog peeps";
-            // Variable: an instance of an object. "Text" is a variable, but it can be anything
-            // Useful if you want to use the same string multiple times (now you can just write "text")
-            // Also, if you want to change the string, you only have to do it in one place
-
-            text += " a new string";
-            // text = text + " a new string";
-            // += is shorthand for saying "old variable plus whatever comes in front of the operator (whatever comes next)
-            // It updates the variable
-
-            // text -= " a thrid string";
-            // this makes an error but you can also minus a section
-            // Also have *= and /=
-            Console.WriteLine(text);
+            string line = Console.ReadLine();
+            Console.WriteLine(line);
+            // console.readline was run, so nothing happened until I hit enter
+            // When I did, it returned what I had written in the program
+            // ReadLine returns code, while WriteLine does not
         }
     }
 }
