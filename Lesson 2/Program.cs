260b9b4c43664e1d14f2dde2fd33bab79338e6b2
BethCushnie <github@bethcushnie.com>
2020-03-10 20:31:40 -0400

    A simple calculator

--- Lesson 2/Program.cs
+++ Lesson 2/Program.cs
@@ -6,9 +6,19 @@ namespace Lesson_2
     {
         static void Main()
         {
-            int x = 10;
-            int y = 20;
-            // int stands for integer
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            // This will make the forground (the text) magenta :)
+           
+            Console.WriteLine("Type a number!");
+            string line = Console.ReadLine();
+            int x = int.Parse(line);
+            // line is a string, but x is an integer
+            // int.Parse() is a function that converts a string into an integer
+            // However, if the string isn't a number, then an error will appear
+            Console.WriteLine("Type another number!");
+            string line2 = Console.ReadLine();
+            int y = int.Parse(line2);
+            Console.WriteLine("The sum of your two numbers is");
             Console.WriteLine(x + y);
         }
     }
