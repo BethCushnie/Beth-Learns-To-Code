ea193bcabe865260d3e1b4e314b40ee278e50a72
BethCushnie <github@bethcushnie.com>
2020-03-22 19:47:20 -0400

    Made a simple calculator thing as a warm up (first draft)

--- Lesson 3/Program.cs
+++ Lesson 3/Program.cs
@@ -4,9 +4,20 @@ namespace Lesson_3
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            
+            Console.WriteLine("Enter a number");
+            string text = Console.ReadLine();
+
+            int number = int.Parse(text);
+            const int goalNumber = 69;
+            // Adding const means that this variable cannot be changed for the rest of the code
+            // Without it, you could assign the variable a different value partway through the code. Adding const prevents this
+
+            int answer = goalNumber - number;
+            Console.WriteLine($"Your number is {answer} away from {goalNumber}");
         }
     }
 }
