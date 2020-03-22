203f0db0d27d53497cdf9119cf5349abbd02f3dc
BethCushnie <github@bethcushnie.com>
2020-03-22 19:29:01 -0400

    created lesson 3 project

---
+++ Lesson 3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
