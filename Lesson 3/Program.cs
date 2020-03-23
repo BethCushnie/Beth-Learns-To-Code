b58d6d404dbac06ac451219ae1f885cb32ebcd21
BethCushnie <github@bethcushnie.com>
2020-03-22 20:51:40 -0400

    not infinite anymore losers

--- Lesson 3/Program.cs
+++ Lesson 3/Program.cs
@@ -7,16 +7,15 @@ namespace Lesson_3
         static void Main()
         {
             int i = 0;
+
             while (i < 5)
             {
                 WriteLine(i);
+                i = i + 1;
             }
-            // Right now, this is an infinite loop. Because i is always < 5, the program just runs a bunch of 0s, forever
         }
 
         static void WriteLine(object line)
-            // Previously, when the variable was a string, this function could only write strings
-            // However, everything in C# is an object, so now WriteLine can be used for anything
         {
             Console.ForegroundColor = ConsoleColor.Cyan;
             Console.WriteLine(line);
