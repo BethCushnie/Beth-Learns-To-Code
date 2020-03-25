e0202fb37a47f595bf932a3f1d2cfcf9021f835c
BethCushnie <github@bethcushnie.com>
2020-03-24 20:10:54 -0400

    warmup

--- Lesson 4/Program.cs
+++ Lesson 4/Program.cs
@@ -4,9 +4,27 @@ namespace Lesson_4
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("How many numbers are you going to enter?");
+
+            int numberOfNumbers = int.Parse(Console.ReadLine());
+
+            int i = 0;
+            int answer = 0;
+            int previousAnswer = 0;
+
+            while (i < numberOfNumbers)
+            {
+                Console.WriteLine("Enter a number");
+                int enteredNumber = int.Parse(Console.ReadLine());
+
+                previousAnswer = answer;
+                answer = previousAnswer + enteredNumber;
+
+                i++;
+            }
+            Console.WriteLine($"The sum of these numbers is {answer}");
         }
     }
 }
