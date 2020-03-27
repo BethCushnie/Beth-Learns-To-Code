457c68895be57d20c73cbc418bd602446966614e
BethCushnie <github@bethcushnie.com>
2020-03-26 20:39:51 -0400

    added some functions

--- Lesson 5/Program.cs
+++ Lesson 5/Program.cs
@@ -4,9 +4,30 @@ namespace Lesson_5
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("How many prime numbers do you want?");
+            int numberOfPrimes = GetNumberFromUser();
+        }
+
+        static void Print(object obj)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(obj);
+        }
+
+        static int GetNumberFromUser()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            bool numberIsValid = int.TryParse(Console.ReadLine(), out int enteredNumber);
+
+            if (numberIsValid)
+                return enteredNumber;
+
+            Print("C'mom man. Number. A NUMBER.");
+            Print("I'm just going to pretend you entered zero because you SUCK and that's the score I give you out of 10.");
+            return 0;
+
         }
     }
 }
