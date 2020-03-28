4dd6469977ed6fd008e3ae14e8677dae029fe73b
BethCushnie <github@bethcushnie.com>
2020-03-28 16:50:36 -0400

    printing primes

--- Lesson 6/Program.cs
+++ Lesson 6/Program.cs
@@ -4,9 +4,38 @@ namespace Lesson_6
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Print("How many squares do you want?");
+            int numberOfSquares = GetNumberFromUser();
+
+            for (int i = 1; i <= numberOfSquares; i++)
+            {
+                Print($"The square of {i} is {SquareOf(i)}");
+            }
+        }
+
+        static int SquareOf(int number)
+        {
+            return number * number;
+        }
+
+        static int GetNumberFromUser()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            bool numberIsTrue = int.TryParse(Console.ReadLine(), out int enteredNumber);
+            
+            if (numberIsTrue)
+                return enteredNumber;
+
+            Print("That's not a number. Returning 0.");
+            return 0;
+        }
+
+        static void Print(object obj)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(obj);
         }
     }
 }
